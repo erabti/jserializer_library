45ca0a20eb2c7b08786bed713cfffbce4177e9fc
Ahmed Erabti <a.erabti@email.ly>
2022-02-14 13:34:14 +0200

    add num to bool adapter

--- jserializer/lib/src/core/adapters/adapters.dart
+++ jserializer/lib/src/core/adapters/adapters.dart
@@ -1,10 +1,12 @@
+import 'num_to_bool.dart';
 import 'number.dart';
 import 'string.dart';
 
+export 'num_to_bool.dart';
 export 'number.dart';
 export 'string.dart';
 
-class JAdapters {
+abstract class JAdapters {
   const factory JAdapters.num({num? fallback, bool handleBool}) = JNumAdapter;
 
   const factory JAdapters.numNullable({num? fallback, bool handleBool}) =
@@ -24,4 +26,9 @@ class JAdapters {
   const factory JAdapters.string() = JStringAdapter;
 
   const factory JAdapters.stringNullable() = JStringNullableAdapter;
+
+  const factory JAdapters.numToBool({bool? fallback}) = JNumToBoolAdapter;
+
+  const factory JAdapters.numToBoolNullable({bool? fallback}) =
+      JNumToBoolNullableAdapter;
 }
---
+++ jserializer/lib/src/core/adapters/num_to_bool.dart
@@ -0,0 +1,42 @@
+import 'package:jserializer/jserializer.dart';
+
+class JNumToBoolAdapter extends CustomAdapter<bool, num> implements JAdapters {
+  const JNumToBoolAdapter({this.fallback});
+
+  final bool? fallback;
+
+  @override
+  bool fromJson(num json) {
+    final n = json.toDouble();
+    if (n == 0.0) return false;
+    if (n == 1.0) return true;
+    if (fallback != null) return fallback!;
+
+    throw FormatException('Cannot convert num ($json) to bool');
+  }
+
+  @override
+  num toJson(bool model) => model ? 1 : 0;
+}
+
+class JNumToBoolNullableAdapter extends CustomAdapter<bool?, num?>
+    implements JAdapters {
+  const JNumToBoolNullableAdapter({this.fallback});
+
+  final bool? fallback;
+
+  @override
+  bool? fromJson(num? json) {
+    final n = json?.toDouble();
+    if (n == 0.0) return false;
+    if (n == 1.0) return true;
+    return fallback!;
+  }
+
+  @override
+  num? toJson(bool? model) {
+    if (model == null) return null;
+
+    return model ? 1 : 0;
+  }
+}
