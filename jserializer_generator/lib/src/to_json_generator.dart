fdaa832c64b78c379fbd3e8810b0492b50269c14
Ahmed Erabti <a.erabti@email.ly>
2023-08-26 23:36:46 +0300

    feat: ToJsonGenerator

---
+++ jserializer_generator/lib/src/to_json_generator.dart
@@ -0,0 +1,100 @@
+import 'package:code_builder/code_builder.dart';
+import 'package:jserializer/jserializer.dart';
+import 'package:jserializer_generator/src/core/j_field_config.dart';
+import 'package:jserializer_generator/src/core/model_config.dart';
+
+class ToJsonGenerator {
+  ToJsonGenerator({
+    required this.modelConfig,
+    required this.config,
+  });
+
+  final ModelConfig modelConfig;
+  final JSerializable config;
+
+  bool get isGeneric => modelConfig.hasGenericValue;
+
+  List<Method> getMethods() => [
+        _getToJson(),
+      ];
+
+  Expression _addAdaptersIfNeeded(JFieldConfig f, Expression exp) {
+    for (final adapter in f.customAdapters) {
+      exp = refer(adapter.adapterFieldName).property('toJson').call([exp]);
+    }
+
+    return exp;
+  }
+
+  bool shouldFilterNulls(JFieldConfig f) =>
+      f.fieldType.isNullable && config.filterToJsonNulls!;
+
+  Expression _getPropertyCall(JFieldConfig f) {
+    final filterNulls = shouldFilterNulls(f);
+
+    return filterNulls
+        ? refer('model').property(f.fieldName).nullChecked
+        : refer('model').property(f.fieldName);
+  }
+
+  MapEntry<Object, Object?> _fieldToCode(JFieldConfig f) {
+    final filterNulls = shouldFilterNulls(f);
+
+    final key = filterNulls
+        ? CodeExpression(
+            Code("if(model.${f.fieldName} != null) '${f.jsonKey}'"),
+          )
+        : literalString(f.jsonKey);
+
+    final exp = _getPropertyCall(f);
+
+    if (f.hasCustomAdapters) {
+      return MapEntry(key, _addAdaptersIfNeeded(f, exp));
+    } else if (!f.fieldType.isPrimitive) {
+      final jSerializerCall =
+          refer('jSerializer').property('toJson').call([exp]).code;
+      return MapEntry(key, jSerializerCall);
+    }
+
+    return MapEntry(key, exp);
+  }
+
+  Method _getToJson() {
+    final mapEntries = modelConfig.fields.map(_fieldToCode);
+
+    Expression body = literalMap(Map.fromEntries(mapEntries));
+
+    if (modelConfig.extrasField != null) {
+      if (modelConfig.extrasField!.keyConfig.overridesFields) {
+        body = body.cascade('addAll').call(
+          [
+            refer('model').property(modelConfig.extrasField!.fieldName),
+          ],
+        );
+      } else {
+        body = refer('model')
+            .property(modelConfig.extrasField!.fieldName)
+            .cascade('addAll')
+            .call(
+          [body],
+        );
+      }
+    }
+
+    return Method(
+      (b) => b
+        ..name = 'toJson'
+        ..annotations.add(refer('override'))
+        ..returns = refer('Map<String, dynamic>')
+        ..lambda = true
+        ..body = body.code
+        ..requiredParameters.add(
+          Parameter(
+            (b) => b
+              ..name = 'model'
+              ..type = modelConfig.type.baseRefer,
+          ),
+        ),
+    );
+  }
+}
