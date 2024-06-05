2594d1ceae1f602c7f19ebf00e7f340e4e106ea9
Ahmed Erabti <a.erabti@prestoeat.com>
2024-06-05 23:06:13 +0200

    make jserializer more resiliant to common serialization errors from backend

--- jserializer/lib/src/core/jserializer/jserializer_impl.dart
+++ jserializer/lib/src/core/jserializer/jserializer_impl.dart
@@ -68,13 +68,102 @@ class JSerializerImpl extends JSerializerInterface {
         typeOf<void>(): (i) => PrimitiveSerializer<void>(jSerializer: i),
         Null: (i) => PrimitiveSerializer<void>(jSerializer: i),
         dynamic: (i) => PrimitiveSerializer<dynamic>(jSerializer: i),
-        int: (i) => PrimitiveSerializer<int>(jSerializer: i),
-        String: (i) => PrimitiveSerializer<String>(jSerializer: i),
-        bool: (i) => PrimitiveSerializer<bool>(jSerializer: i),
-        num: (i) => PrimitiveSerializer<num>(jSerializer: i),
-        double: (i) => PrimitiveSerializer<double>(jSerializer: i),
+        int: (i) => IntSerializer(jSerializer: i),
+        String: (i) => StringSerializer(jSerializer: i),
+        bool: (i) => BoolSerializer(jSerializer: i),
+        num: (i) => NumSerializer(jSerializer: i),
+        double: (i) => DoubleSerializer(jSerializer: i),
         List: (i) => ListSerializer(jSerializer: i),
         Map: (i) => MapSerializer(jSerializer: i),
       },
     );
+}
+
+class BoolSerializer extends Serializer<bool, dynamic> {
+  const BoolSerializer({super.jSerializer});
+
+  @override
+  dynamic toJson(bool model) => model;
+
+  @override
+  Function get decoder => fromJson;
+
+  bool fromJson(dynamic json) {
+    if (json is bool) return json;
+    if (json is num) return json != 0;
+    if (json is String) return json != 'false' && json != '0';
+
+    return json;
+  }
+}
+
+class NumSerializer extends Serializer<num, dynamic> {
+  const NumSerializer({super.jSerializer});
+
+  @override
+  dynamic toJson(num model) => model;
+
+  @override
+  Function get decoder => fromJson;
+
+  num fromJson(dynamic json) {
+    if (json is num) return json;
+    if (json is String) return num.parse(json);
+
+    return json;
+  }
+}
+
+class StringSerializer extends Serializer<String, dynamic> {
+  const StringSerializer({super.jSerializer});
+
+  @override
+  dynamic toJson(String model) => model;
+
+  @override
+  Function get decoder => fromJson;
+
+  String fromJson(dynamic json) {
+    if (json is String) return json;
+    if (json is num) return json.toString();
+    if (json is bool) return json.toString();
+
+    return json;
+  }
+}
+
+class DoubleSerializer extends Serializer<double, dynamic> {
+  const DoubleSerializer({super.jSerializer});
+
+  @override
+  dynamic toJson(double model) => model;
+
+  @override
+  Function get decoder => fromJson;
+
+  double fromJson(dynamic json) {
+    if (json is double) return json;
+    if (json is num) return json.toDouble();
+    if (json is String) return double.parse(json);
+
+    return json;
+  }
+}
+
+class IntSerializer extends Serializer<int, dynamic> {
+  const IntSerializer({super.jSerializer});
+
+  @override
+  dynamic toJson(int model) => model;
+
+  @override
+  Function get decoder => fromJson;
+
+  int fromJson(dynamic json) {
+    if (json is int) return json;
+    if (json is num) return json.round();
+    if (json is String) return int.parse(json);
+
+    return json;
+  }
 }
